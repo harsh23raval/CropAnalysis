635ab67caf9a99131d888a64e22e368de0ccbddc
harsh23raval <49579010+harsh23raval@users.noreply.github.com>
2020-07-22 19:45:07 +0530

    Update yeild_with_graph_data.py

--- yeild_with_graph_data.py
+++ yeild_with_graph_data.py
@@ -33,7 +33,7 @@ def predict():
     d = {}
 
     #with open("yield_june_to_sep_rainfall.pickle", 'rb') as file:
-    __model = pickle.load("yield_june_to_sep_rainfall.pickle")
+    __model = pickle.load("yield_june_to_sep_rainfall.pickle",'rb')
 
     with open("columns_of_yeild_june_to_sep_rainfall.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
